e213e77322082a903db9a7ebd85f579a2c358dcc
Gulevich Vasili <V.V.Gulevich@mail.ru>
2012-09-25 12:57:53 +0700

    Tree collector now has multihit branches.

--- tree/main.cpp
+++ tree/main.cpp
@@ -15,7 +15,7 @@ typedef V1190BClient::Event::Measurements Measurements;
 
 using namespace std;
 
-TBranch * createBracnh(TTree & tree, const char * name, int * ptr, unsigned size) {
+TBranch * createBranch(TTree & tree, const char * name, int * ptr, unsigned size) {
 	TString format;
 	if (size == 0) 
 		return 0;
@@ -66,14 +66,38 @@ struct Runner {
 		TTree *tree = new TTree("eventTree","rootEvent");
 
 		const int BRANCH_SIZE = 64;
-		int channels[BRANCH_SIZE];
+		int firstTimes[BRANCH_SIZE];
+		Int_t channels[BRANCH_SIZE];
+		Int_t times[BRANCH_SIZE];
+		int hitCount;
 
-		TBranch * branch = createBracnh(*tree, "rawTimes", channels, BRANCH_SIZE);
+
+		TBranch * branch = createBranch(*tree, "firstTimes", firstTimes, BRANCH_SIZE);
 
 		if (!branch) {
-			cerr << "Couldn't create branch" << endl;
+			cerr << "Couldn't create first times branch" << endl;
 			return 5;
 		}
+
+		branch = tree->Branch("hitCount", &hitCount, "hitCount/I");
+		if (!branch) {
+			cerr << "Couldn't create hitCount branch" << endl;
+			return 5;
+		}
+
+		branch = tree->Branch("channels", channels, "channels[hitCount]/I");
+		if (!branch) {
+			cerr << "Couldn't create channels branch" << endl;
+			return 5;
+		}
+
+		branch = tree->Branch("times", times, "times[hitCount]/I");
+		if (!branch) {
+			cerr << "Couldn't create times branch" << endl;
+			return 5;
+		}
+
+
 
 		while (client) {
 			if (!client.readEvent(event)) {
@@ -95,6 +119,17 @@ struct Runner {
 				} else {
 					channels[i->gChannel()] = i->time;
 				}
+			}
+
+			hitCount = 0;
+			for (Measurements::const_iterator i=event.measurements.begin(); i!=event.measurements.end(); i++) {
+				if (hitCount >= BRANCH_SIZE) {
+					cerr << "Event is too big" << endl;
+					break;
+				}
+				channels[hitCount] = i->gChannel();
+				times[hitCount] = i->time;
+				hitCount++;
 			}
 			tree->Fill();
 			if (long(event.count) > _events)
